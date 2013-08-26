2478f3c2cb8720959fe674eb9165031cd4622f4d
praaveen <praaveen@ubuntu.ubuntu-domain>
2013-08-26 08:47:33 +0530

    added edit and update function(error in update)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def edit
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params[:post])
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
 =begin
         private
     def post_params
